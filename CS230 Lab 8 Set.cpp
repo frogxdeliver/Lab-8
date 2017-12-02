ef3f55d7c8d820b021e5f9481a695ff6e75e13f5
zdixonsat <staytuned410@gmail.com>
2017-12-02 16:40:56 -0500

    Add files via upload
    Debugging issue is from lines 41-51. If 'add' is given, the set takes the new item and supposed to print out "Successful Addition!". But, it doesn't. It tried rewriting the nested if statement, but need fresh eyes. 
    Good news the set search portion, user prompt to put insert into set, set iteration and display work.

---
+++ CS230 Lab 8 Set.cpp
@@ -0,0 +1,97 @@
+//this application implements use of a simple set
+//program does data insertion,searching, and deletion
+//program will represent a set of keys
+//a user can add a key, delete a key, and check for duplicate keys
+//GROUP: Zalika, Lukas, Will, and Malik
+#include<iostream>
+#include<set>
+#include<string>
+using namespace std;
+
+int main()
+{
+string key, answer,answer2;
+int i;
+
+std::set<std::string> Keyring;
+
+//prompts user to insert elements in set
+for(i = 0; i < 5; i++)
+{
+cout<<"\nInsert a Key " <<i+1<<" Onto Key Ring"<<endl;
+cin >>key;
+Keyring.insert(key);
+}
+//shows resulting key ring
+cout<<"\nHere is Completed Key Ring"<<endl;
+for(std::set<std::string>::iterator it=Keyring.begin(); it !=Keyring.end(); it++) //the '<' operator keeps UNIQUE elements in sorted order
+{
+std::cout<<" "<<"\n"<< *it; //
+}
+cout<<"\n\nKey Ring Size Is: "<<Keyring.size()<<" Keys"<<endl;
+
+
+
+//Set Deletion and Addition
+cout<<"\nWould You Like to Add or Delete From Key Ring?"<<endl;
+cout<<"\nType add or delete"<<endl;
+cin>>answer;
+if(answer=="add")
+{
+cout<<"\nAdd Another Key. Use _ For Spaces"<<endl;
+cin>>key;
+Keyring.insert(key);
+}
+if(Keyring.insert(key).second)
+{
+cout<<key<<" Successful Addition!"<<endl;
+}
+else
+{
+  cout<<key<<" Duplicate Key Can't Be Added!"<<endl;
+}
+cout<<"\nThe New Keyring is below: \n"<<endl;
+for(std::set<std::string>::iterator it=Keyring.begin(); it !=Keyring.end(); it++)
+{
+std::cout<<" "<<"\n"<< *it; //
+}
+cout<<"\nNew Keyring size is "<<Keyring.size()<<endl;
+
+
+
+if(answer=="delete")
+{
+cout<<"\nDelete A Key. Use _ For Spaces"<<endl;
+cin>>key;
+Keyring.erase(key);
+cout<<"\nNew Keyring"<<endl;
+
+for(std::set<std::string>::iterator it=Keyring.begin(); it !=Keyring.end(); it++)
+{
+std::cout<<" "<<"\n"<< *it;
+}
+}
+cout<<"\nNew Keyring size is "<<Keyring.size()<<endl;
+
+
+
+//Set Searching
+cout<<"\nWant To Search For A Key y/n?"<<endl;
+
+if(answer2=="y")
+{
+cout<<"\nSearch For A Key. Use _ For Spaces"<<endl;
+cout<<"\nWhere Is My..."<<endl;
+cin>>key; cout<<" Key?"<<endl;
+
+std::set<std::string>::iterator it=Keyring.find(key);
+if(it !=Keyring.end())
+{
+cout<<key<<"\nFound!"<<endl;
+}
+else
+	cout<<key<<"\nNot Found :("<<endl;
+}
+
+return 0;
+}
